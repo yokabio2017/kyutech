538629ceda0d7dc2a7d6e143444da97a91e2d7e9
uedaPC\ueda <u_time_leap@yahoo.co.jp>
2017-08-24 17:05:23 +0900

    スマホでもなんとか見れるように

--- js/scroll.js
+++ js/scroll.js
@@ -2,19 +2,23 @@ $(function(){
   $('html, body').hide();
   // URLのアンカー（#以降の部分）を取得
   var urlHash = location.hash;
+  var offset = 50;
 
+  if(window.innerWidth >= 768){
+    offset = 100;
+  }
   // URLにアンカーが存在する場合
   if(urlHash){
     $('html, body').show();
     var speed = 500;
-    var position = $(urlHash).offset().top - $(urlHash).height() - 50;
+    var position = $(urlHash).offset().top - offset;
     $("html, body").animate({scrollTop:position}, speed, "swing");
   }
   $('a[href^="#"]').click(function(){
     var speed = 500;
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top - target.height() - 50;
+    var position = target.offset().top - offset;
     $("html, body").animate({scrollTop:position}, speed, "swing");
     return false;
   });
