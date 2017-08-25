62ed11ef9c894618ec5a08be6456efceea20e36d
uedaPC\ueda <u_time_leap@yahoo.co.jp>
2017-08-25 16:47:14 +0900

    画像大量入荷！！

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,10 +1,4 @@
 $(function(){
   $("#footer").load("footer.html");
-  $("header.main").load("header.html", function(){
-    $("#main-nav li").hover(function() {
-        $("ul:not(:animated)", this).slideDown();
-    }, function() {
-        $(this).children('ul').slideUp();
-    });
-  });
+  
 });
